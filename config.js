d25ad92701338c1fd6bc966438c9cb0994e0de04
WZ BOT OFFICIAL <gamingwan92@gmail.com>
2022-06-28 20:59:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6288975667597','0']
 global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
+global.author = 'Wz Store'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'sanz ganteng'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
